c10fa6bd294707c4d628ac1067bcab5213253c99
John Ross Hunt <johnrosshunt@gmail.com>
2025-09-06 15:20:31 -0500

    Updated display_scrolling-text.py

--- build your own/display_scrolling-text.py
+++ build your own/display_scrolling-text.py
@@ -6,7 +6,7 @@ from PIL import Image
 from PIL import ImageDraw
 from PIL import ImageFont
 
-import ST7789
+import st7789
 
 
 MESSAGE = "Hello World! How are you today?"
@@ -41,11 +41,11 @@ except IndexError:
 # Create ST7789 LCD display class.
 
 if display_type in ("square", "rect", "round"):
-    disp = ST7789.ST7789(
+    disp = st7789.ST7789(
         height=135 if display_type == "rect" else 240,
         rotation=0 if display_type == "rect" else 90,
         port=0,
-        cs=ST7789.BG_SPI_CS_FRONT,  # BG_SPI_CS_BACK or BG_SPI_CS_FRONT
+        cs=st7789.BG_SPI_CS_FRONT,  # BG_SPI_CS_BACK or BG_SPI_CS_FRONT
         dc=9,
         backlight=19,               # 18 for back BG slot, 19 for front BG slot.
         spi_speed_hz=80 * 1000 * 1000,
@@ -54,7 +54,7 @@ if display_type in ("square", "rect", "round"):
     )
 
 elif display_type == "dhmini":
-    disp = ST7789.ST7789(
+    disp = st7789.ST7789(
         height=240,
         width=320,
         rotation=180,
@@ -83,7 +83,8 @@ draw = ImageDraw.Draw(img)
 
 font = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf", 30)
 
-size_x, size_y = draw.textsize(MESSAGE, font)
+left, _, right, bottom = font.getbbox(MESSAGE)
+size_x, size_y = right - left, bottom
 
 text_x = disp.width
 text_y = (disp.height - size_y) // 2
